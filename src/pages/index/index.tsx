98905de75174616d8f76f185e5a4f9ec068e19e6
wangjie03 <wangjie03@zhidemai.com>
2023-07-20 13:50:18 +0800

    no message

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import { View } from '@tarojs/components'
-import { Button } from "@nutui/nutui-react-taro"
-import './index.less'
+import { useState } from 'react'
+import { View, WebView } from '@tarojs/components'
+// import './index.less'
 
 function Index() {
+  const [nodes, setNodes] = useState(
+    [{
+      name: 'div',
+      attrs: {
+        class: 'div_class',
+        style: 'line-height: 60px; color: red;'
+      },
+      children: [{
+        type: 'text',
+        text: 'Hello World!'
+      }]
+    }]
+  )
+
+  function onEditorReady() {
+    console.log('editor ready')
+  }
+
   return (
     <View className="nutui-react-demo">
-      <View className="index">
-        欢迎使用 NutUI React 开发 Taro 多端项目。
-      </View>
-      <View className="index">
-        <Button type="primary" className="btn">
-          NutUI React Button
-        </Button>
-      </View>
+      {/* 小程序下 webview的承载 */}
+      <WebView  src='http://10.150.14.247:10086/#/pages/editor/index'/>
     </View>
   )
 }
